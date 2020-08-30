7959bd6c75dbba9c3c6a8afaa2d55d50c9d89b8d
Mitchell Moser <moser.mitch@yahoo.com>
2020-08-30 18:44:31 -0500

    Add files via upload

--- LACheck/Program.cs
+++ LACheck/Program.cs
@@ -4,6 +4,10 @@ using System.IO;
 using System.Collections.Generic;
 using System.DirectoryServices.AccountManagement;
 using System.Threading;
+using WSManAutomation;
+
+using System.Linq;
+using System.Xml.Linq;
 
 namespace LACheck
 {
@@ -12,19 +16,8 @@ namespace LACheck
         static void Main(string[] args)
         {
             var parsedArgs = ParseArgs(args);
-            PrintOptions(parsedArgs);
-            if (parsedArgs.ContainsKey("help"))
-            {
-                Usage();
-                Environment.Exit(1);
-            }
-            if (!parsedArgs.ContainsKey("smb") && !parsedArgs.ContainsKey("rpc"))
-            {
-                Console.WriteLine("[!] No check type specified: smb rpc");
-                Usage();
-                Environment.Exit(1);
-            }
-
+            ValidateArguments(parsedArgs);
+            
             bool verbose = false;
             if (parsedArgs.ContainsKey("/verbose"))
             {
@@ -54,7 +47,7 @@ namespace LACheck
             {
                 validate = Convert.ToBoolean(parsedArgs["/validate"][0]);
             }
-
+            PrintOptions(parsedArgs, rpc, smb, winrm);
             if (validate)
             {
                 ValidateCredentials();
@@ -66,7 +59,13 @@ namespace LACheck
                 hosts = parsedArgs["/targets"][0].Split(',');
             }
 
+            //https://docs.microsoft.com/en-us/windows/win32/wmisdk/wql-operators
+            //https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/win32-computersystem
+            //https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/win32-logonsession
+            //string wql = "Select * from Win32_LogonSession Where LogonType = 10";
+            string wql = "SELECT PartOfDomain FROM Win32_ComputerSystem";
             string ns = @"root\cimv2";
+            
             foreach (string host in hosts)
             {
                 try
@@ -76,10 +75,10 @@ namespace LACheck
                         Console.WriteLine("[+] Connecting to {0}", host);
                     }
                     //RPC Check
-                    if (smb)
+                    if (rpc)
                     {
                         //https://stackoverflow.com/questions/1195896/threadstart-with-parameters
-                        Thread newThread = new Thread(() => RPC_Check(host, ns, verbose));
+                        Thread newThread = new Thread(() => RPC_Check(host, ns, wql, verbose));
                         newThread.Start();
                     }
                     
@@ -91,14 +90,12 @@ namespace LACheck
                     }
 
                     //WinRM Check
-                    /*
                     if (winrm)
                     {
                         //https://stackoverflow.com/questions/1195896/threadstart-with-parameters
-                        Thread newThread = new Thread(() => WinRM_Check(host, verbose));
+                        Thread newThread = new Thread(() => WinRM_Check(host, wql, verbose));
                         newThread.Start();
                     }
-                    */
                 }
                 catch (Exception ex)
                 {
@@ -110,30 +107,60 @@ namespace LACheck
                 }
             }
         }
-        /*
-        static void WinRM_Check(string host, bool verbose)
+
+        static void WinRM_Check(string host, string wql, bool verbose)
         {
-            //https://bohops.com/2020/05/12/ws-management-com-another-approach-for-winrm-lateral-movement/
-            IWSManEx wsman = new WSMan();
-            IWSManConnectionOptions options = (IWSManConnectionOptions)wsman.CreateConnectionOptions();
-            //string sessionURL = Globals.protocol + "://" + host + ":" + Globals.port + "/wsman";
-            string sessionURL = "https://" + host + ":5985/wsman";
-            IWSManSession session = (IWSManSession)wsman.CreateSession(sessionURL, 0, options);
-            string resource = "http://schemas.microsoft.com/wbem/wsman/1/wmi/root/cimv2/Win32_Process";
-            string parameters = "<p:Create_INPUT xmlns:p=\"http://schemas.microsoft.com/wbem/wsman/1/wmi/root/cimv2/Win32_Process\"><p:CommandLine>notepad.exe</p:CommandLine></p:Create_INPUT>";
-            string response = session.Invoke("Create", resource, parameters);
+            try
+            {
+                //https://bohops.com/2020/05/12/ws-management-com-another-approach-for-winrm-lateral-movement/
+                //https://github.com/bohops/WSMan-WinRM/blob/master/SharpWSManWinRM.cs
+                IWSManEx wsman = new WSMan();
+                IWSManConnectionOptions options = (IWSManConnectionOptions)wsman.CreateConnectionOptions();
+                IWSManSession session = (IWSManSession)wsman.CreateSession(host, 0, options);
+
+                //https://docs.microsoft.com/en-us/windows/win32/winrm/querying-for-specific-instances-of-a-resource
+                //https://stackoverflow.com/questions/29645896/how-to-retrieve-cim-instances-from-a-linux-host-using-winrm
+                string resource = "http://schemas.microsoft.com/wbem/wsman/1/wmi/root/cimv2/*";
+                string dialect = "http://schemas.microsoft.com/wbem/wsman/1/WQL";
+                IWSManEnumerator response = session.Enumerate(resource, wql, dialect);
+                // Enumerate returned CIM instances.
+                string results = "";
+                while (!response.AtEndOfStream)
+                {
+                    string item = response.ReadItem();
+                    XDocument doc = XDocument.Parse(item);
+                    var resultSet = from e in doc.Elements() select e;
+
+                    foreach (var element in resultSet)
+                    {
+                        results += element;
+                    }
+                }
+                if (results != "")
+                {
+                    Console.WriteLine("[WinRM] Admin Succes: {0}", host);
+                }
+                if (verbose && results == "")
+                {
+                    Console.WriteLine("[!] WinRM no response for query on {0}", host);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (verbose)
+                {
+                    Console.WriteLine("[!] WinRM on {0} - {1}", host, ex.Message);
+                }
+            }
+
         }
-        */
-        static void RPC_Check(string host, string ns, bool verbose)
+
+        static void RPC_Check(string host, string ns, string wql, bool verbose)
         {
             try
             {
                 ManagementScope scope = new ManagementScope(string.Format(@"\\{0}\{1}", host, ns));
-                //https://docs.microsoft.com/en-us/windows/win32/wmisdk/wql-operators
-                //https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/win32-computersystem
-                //https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/win32-logonsession
-                //Select * from Win32_LogonSession Where LogonType = 10
-                SelectQuery query = new SelectQuery("SELECT PartOfDomain FROM Win32_ComputerSystem");
+                SelectQuery query = new SelectQuery(wql);
                 scope.Connect();
                 using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query))
                 {
@@ -208,8 +235,9 @@ Usage:
     LACheck.exe smb rpc /targets:hostname,fqdn.domain.tld,10.10.10.10 /verbose /validate
 
 Local Admin Checks:
-    smb - Attempts to access C$ share
-    rpc - Attempts WMI query of Win32_ComputerSystem Class provider
+    smb   - Attempts to access C$ share
+    rpc   - Attempts WMI query of Win32_ComputerSystem Class provider over RPC
+    winrm - Attempts WMI query of Win32_ComputerSystem Class Provider over WinRM Session
 
 Argument:
     /targets  - comma-separated list of hostnames to check. If none provided, localhost will be checked.
@@ -219,32 +247,45 @@ Argument:
             Console.WriteLine(usageString);
         }
 
-        static void PrintOptions(Dictionary<string, string[]> args)
+        static void PrintOptions(Dictionary<string, string[]> args, bool smb, bool rpc, bool winrm)
         {
             Console.WriteLine("[+] Parsed Aguments:");
+            if (!args.ContainsKey("smb"))
+            {
+                Console.WriteLine("\tsmb: false");
+            }
+            if (!args.ContainsKey("rpc"))
+            {
+                Console.WriteLine("\trpc: false");
+            }
+            if (!args.ContainsKey("winrm"))
+            {
+                Console.WriteLine("\twinrm: false");
+            }
             foreach (string key in args.Keys)
             {
                 Console.WriteLine("\t{0}: {1}", key, string.Join(", ", args[key]));
             }
+
         }
-        static bool ValidateArguments(Dictionary<string, string[]> args)
+        static void ValidateArguments(Dictionary<string, string[]> args)
         {
             if (args.ContainsKey("help"))
             {
-                return false;
+                Usage();
+                Environment.Exit(0);
             }
-            if (!args.ContainsKey("smb") && !args.ContainsKey("rpc"))
+            if (!args.ContainsKey("smb") && !args.ContainsKey("rpc") && !args.ContainsKey("winrm"))
             {
-                Console.WriteLine("[!] No check type specified: smb rpc");
-                return false;
+                Console.WriteLine("[!] No check type specified: smb rpc winrm");
+                Environment.Exit(0);
             }
-            return true;
         }
         static Dictionary<string, string[]> ParseArgs(string[] args)
         {
             Dictionary<string, string[]> result = new Dictionary<string, string[]>();
             //these boolean variables aren't passed w/ values. If passed, they are "true"
-            string[] booleans = new string[] { "smb", "wmi", "/verbose" };
+            string[] booleans = new string[] { "smb", "wmi", "winrm", "/verbose" };
             var argList = new List<string>();
             foreach (string arg in args)
             {
@@ -255,7 +296,7 @@ Argument:
                 //boolean variables
                 if (parts.Length == 1)
                 {
-                    result[parts[0]] = new string[] { "True" };
+                    result[parts[0]] = new string[] { "true" };
                 }
                 if (parts.Length == 2)
                 {
