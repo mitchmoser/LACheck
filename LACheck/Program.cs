3d16828eac585379c5d91228bdb7d559acb3629f
Mitchell Moser <moser.mitch@yahoo.com>
2020-12-08 19:52:52 -0600

    improved LDAP search efficiency

--- LACheck/Program.cs
+++ LACheck/Program.cs
@@ -264,7 +264,7 @@ namespace LACheck
 
                 DirectoryEntry entry = new DirectoryEntry();
                 DirectorySearcher mySearcher = new DirectorySearcher(entry);
-
+                mySearcher.PropertiesToLoad.Add("samaccountname");
                 //https://social.technet.microsoft.com/wiki/contents/articles/5392.active-directory-ldap-syntax-filters.aspx
                 //https://ldapwiki.com/wiki/Active%20Directory%20Computer%20Related%20LDAP%20Query
                 switch (filter)
@@ -298,16 +298,16 @@ namespace LACheck
                 
                 mySearcher.SizeLimit = int.MaxValue;
                 mySearcher.PageSize = int.MaxValue;
-                int counter = 0;
                 foreach (SearchResult resEnt in mySearcher.FindAll())
                 {
                     string ComputerName = resEnt.GetDirectoryEntry().Name;
                     if (ComputerName.StartsWith("CN="))
                         ComputerName = ComputerName.Remove(0, "CN=".Length);
                     ComputerNames.Add(ComputerName);
-                    counter += 1;
                 }
-                Console.WriteLine("[+] LDAP Search Results: {0}", counter.ToString());
+                //localhost returns false positives
+                ComputerNames.RemoveAll(u => u.Contains(System.Environment.MachineName));
+                Console.WriteLine("[+] LDAP Search Results: {0}", ComputerNames.Count.ToString());
                 mySearcher.Dispose();
                 entry.Dispose();
 
